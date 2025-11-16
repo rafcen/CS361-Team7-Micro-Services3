52bca0d81fd1826457bd875c10eb149d0f50518a
scooggins <scoggins.trent@gmail.com>
2025-11-15 18:12:34 -0600

    Updated readme, working server, woring test client, test folder of images

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# CS361-Team7-Micro-Services3
+# CS361 Microservice 3: Image Microservice
+#
+# Send the server a folder path, it will reply with a dictionary of the images
+# in the folder with the key being the image name and value being the path to the image.
+# The image microservice sorts through the given folder to pick images files of type
+# jpeg/jpg, png, and webp. If there are no images of those types, or if the folder is
+# empty, the server will print an error to the console and destroy itself.
+# 
+# Communication Contract
+# REQUEST parameters: folder_path
+# Example Call:
+#   socket.send_string(folder_path)
+#
+# RECEIVE parameters: image_dictionary
+# Example Call:
+#   socket.send_json(image_dictionary)
---
+++ client.py
@@ -0,0 +1,28 @@
+# CS361 Microservice 3: Image Microservice
+# Trent Scoggins
+# Used Assignment 4 as a starting point, heavily modified after.
+
+import os, zmq
+
+# Path to images (generated in main program/additional microservice)
+folder_path = os.getcwd() +  "/team-fortress-2"
+
+# set up the environment 
+context = zmq.Context()
+print("Attempting to connect to server")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5002")
+
+# Send folder path for server to package images
+print(f"Sending parameters: /team-fortress-2")
+socket.send_string(folder_path)
+
+# receive confirmation from server
+return_dict = socket.recv_pyobj()
+# print out confirmation
+print(f"Dictionary recieved, verifying integrity")
+for key, value in return_dict.items():
+    print(f"{key}: {value}")
+
+# Close connection to server
+socket.send_string("Q") 
